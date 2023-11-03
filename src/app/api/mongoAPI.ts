d1e07b8b225e1756a672b3c9a232585f070eb070
Oleksandr Kutsenko <104622826+Fstv-Electro@users.noreply.github.com>
2023-11-03 21:47:50 +0200

    change base url axios

--- src/app/api/mongoAPI.ts
+++ src/app/api/mongoAPI.ts
@@ -2,7 +2,8 @@ import axios from "axios";
 
 
 const api = axios.create({
-    baseURL: 'http://localhost:3000/api'
+    // baseURL: 'http://localhost:3000/api'
+    baseURL: 'http://fstv-electro.github.io/admin-banner/api'
 });
 
 const mongoAPI = {
