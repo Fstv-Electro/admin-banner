38e41772b92fde6140a84199c797bf7e9b5554e1
Oleksandr Kutsenko <104622826+Fstv-Electro@users.noreply.github.com>
2023-11-04 11:48:28 +0200

    change settings

--- dist/server/server-reference-manifest.js
+++ dist/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"xZkZrlh0UucN298Tl5F648xvoh4FthRvOGVCq1tv6WI=\"}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"WmOqAEquDzbiJ/szOzCbA0ld/0NAZ/LvdQ1Lp+6Pe6Y=\"}"
--- next.config.js
+++ next.config.js
@@ -1,9 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
-  experimental: {
-    appDir: true,
-    serverComponentsExternalPackages: ["mongoose"],
+  distDir: "dist",
+  images: {
+    unoptimized: true,
   },
 };
 
